d56c5f86067c3c5780eeda48bcccb4982b1b1154
Lucas Evangelista <lucas1994@gmail.com>
2023-10-04 18:10:39 -0300

    fixing customer request and spec

--- contract-testing/request/customer.request.js
+++ contract-testing/request/customer.request.js
@@ -7,9 +7,8 @@ const baseUrl = `http://localhost:${process.env.MOCK_PORT}`;
 export const customerList = async () => {
   return await axios.post(`${baseUrl}/graphql`, data, {
     headers: {
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiaWF0IjoxNjk1MDYwNDI5LCJleHAiOjE2OTUyMzMyMjl9.5WZD_dmL9jV7SsXevI-tKBDPRxjzs0BPmtBrUDD5Muk",
-      "Content-Type": "application/json",
+      Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiaWF0IjoxNjk2NDQ5NzQyLCJleHAiOjE2OTY2MjI1NDJ9.kJcFKxwgE1d2jx2CnxY7Iq_QLPIIfd3ko7F2epFyi8w",
+      "Content-Type": 'application/json',
     },
   });
 };
--- contract-testing/test/customer.spec.js
+++ contract-testing/test/customer.spec.js
@@ -24,81 +24,40 @@ describe("Contract Costumer Test", () => {
           method: "POST",
           path: "/graphql",
           headers: {
-            Authorization:
-              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiaWF0IjoxNjk1MDYwNDI5LCJleHAiOjE2OTUyMzMyMjl9.5WZD_dmL9jV7SsXevI-tKBDPRxjzs0BPmtBrUDD5Muk",
-            "Content-Type": "application/json",
+            Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiaWF0IjoxNjk2NDQ5NzQyLCJleHAiOjE2OTY2MjI1NDJ9.kJcFKxwgE1d2jx2CnxY7Iq_QLPIIfd3ko7F2epFyi8w",
+            "Content-type": 'application/json',
           },
           body: {
-            query: `query {
-							customers {
-								address {
-								address_1,
-								address_2,
-								city,
-								createdAt,
-								customers: id,
-								id,
-								state,
-								updatedAt,
-								zip
-							},
-							createdAt,
-							email,
-							firstName,
-							id,
-							lastName,
-							orders {
-									createdAt,
-									customer: id,
-									discount,
-									id,
-									product {
-										createdAt,
-										description,
-										id,
-										itemPrice,
-										name,
-										orders: id ,
-										updatedAt
-									},
-									quantity,
-									totalPrice,
-									updatedAt
-								},
-							phone,
-							updatedAt
-						}
-						}
-							`,
+            query: "query{customers{address{address_1,address_2,city,createdAt,customers:id,id,state,updatedAt,zip},createdAt,email,firstName,id,lastName,orders{createdAt,customer:id,discount,id,product{createdAt,description,id,itemPrice,name,orders:id,updatedAt},quantity,totalPrice,updatedAt},phone,updatedAt}}",
           },
         },
         willRespondWith: {
           status: 200,
           headers: {
-            "Content-Type": "application/json",
+            "Content-type": "application/json",
           },
           body: {
             data: {
               customers: eachLike({
                 address: {
-                  address_1: somethingLike("Braga Alameda"),
-                  address_2: somethingLike("Suite 358"),
-                  city: somethingLike("Moraes do Sul"),
-                  createdAt: somethingLike("2023-09-15T22:07:46.096Z"),
-                  customers: somethingLike("clml5hlf4000ei12snpvpnrbg"),
-                  id: somethingLike("clml5hlf4000ei12snpvpnrbg"),
-                  state: somethingLike("Rio Grande do Norte"),
-                  updatedAt: somethingLike("2023-09-15T22:07:46.096Z"),
-                  zip: somethingLike(23944),
+                  address_1: somethingLike("Reis Rodovia"),
+                  address_2: somethingLike("Suite 260"),
+                  city: somethingLike("Hormino do Norte"),
+                  createdAt: somethingLike("2023-10-04T20:07:39.767Z"),
+                  customers: somethingLike("clnc6kbna0000i1l0ofuq2utl"),
+                  id: somethingLike("clnc6kbna0000i1l0ofuq2utl"),
+                  state: somethingLike("Amazonas"),
+                  updatedAt: somethingLike("2023-10-04T20:07:39.767"),
+                  zip: somethingLike(55971605),
                 },
-                createdAt: somethingLike("2023-09-15T22:07:46.113Z"),
-                email: somethingLike("Mrcia.Saraiva81@bol.com.br"),
-                firstName: somethingLike("Tânia"),
-                id: somethingLike("clml5hlfk000gi12sn2bfvs06"),
-                lastName: somethingLike("Martins"),
+                createdAt: somethingLike("2023-10-04T20:07:52.614Z"),
+                email: somethingLike("lucas@ebac.com"),
+                firstName: somethingLike("Lucas"),
+                id: somethingLike("clnc6klk60002i1l0ftdfl2hu"),
+                lastName: somethingLike("Evangelista"),
                 orders: somethingLike([]),
-                phone: somethingLike("(44) 06866-5337"),
-                updatedAt: somethingLike("2023-09-15T22:07:46.113Z"),
+                phone: somethingLike("71999999999"),
+                updatedAt: somethingLike("2023-10-04T20:07:52.614Z"),
               }),
             },
           },
@@ -107,21 +66,22 @@ describe("Contract Costumer Test", () => {
     });
   });
 
-  afterAll(() => {
-    mockProvider.finalize();
+  afterAll(async () => {
+    await mockProvider.finalize();
   });
 
-  afterEach(() => {
-    mockProvider.verify();
+  afterEach(async () => {
+    await mockProvider.verify();
   });
 
-  it("should return consumer list", () => {
-    customerList().then((response) => {
-      const { firstName, lastName } = response.data.data.customers[3];
+  it("should return consumer list", async () => {
+    await customerList().then((response) => {
+      const { firstName, lastName } = response.data.data.customers[0];
+      console.log(firstName, lastName);
 
       expect(response.status).toEqual(200);
-      expect(firstName).toBe("Tânia");
-      expect(lastName).toBe("Martins");
+      expect(firstName).toBe("Lucas");
+      expect(lastName).toBe("Evangelista");
     });
   });
 });

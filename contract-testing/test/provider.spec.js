4eea59316e9d57c8dce5d586bc9ed1475b1fca99
Lucas Evangelista <lucas1994@gmail.com>
2023-10-05 23:53:34 -0300

    adding provider spec

---
+++ contract-testing/test/provider.spec.js
@@ -0,0 +1,17 @@
+import { Verifier } from "@pact-foundation/pact";
+import "dotenv/config";
+
+describe("Pact Verification", () => {
+  it("Should validate the pact", () => {
+    const brokerOpts = {
+      provider: "ebac-demo-store-server",
+      providerBaseUrl: process.env.PROVIDER_URL,
+      pactUrls: [
+        "http://localhost:9292/pacts/provider/ebac-demo-store-server/consumer/ebac-demo-store-admin/latest",
+      ],
+      publishVerificationsResult: true,
+      providerVersion: "1.0.0",
+    };
+    return new Verifier(brokerOpts).verifyProvider();
+  });
+});
